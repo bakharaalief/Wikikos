b07f42a06a4112e5b8ec89dcc4f0c6557a3da80d
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2021-04-06 09:04:44 +0700

    nambahin di create kos

---
+++ js/create-kosan.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    if (jQuery) {  
+      // jQuery is loaded  
+      alert("Yeah!");
+    } else {
+      // jQuery is not loaded
+      alert("Doesn't Work");
+    }
+ });
--- pages/create-kos.php
+++ pages/create-kos.php
@@ -1,9 +1,9 @@
 <div class="container" id="create-kosan">
     <h1>Buat Kosan</h1>
-    <form action="" method="Post">
-        <div class="row align-items-start">
-            <!-- info kosan -->
-            <div class="col">
+    <div class="row align-items-start">
+        <!-- info kosan -->
+        <div class="col">
+            <form action="" method="Post">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Nama</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -28,23 +28,51 @@
                     <label for="exampleInputPassword1">Lokasi</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
-            </div>
-            <!-- gambar dan fasilitas -->
-            <div class="col">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Fasilitas</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <button class="btn btn-primary">Tambah Fasilitas</button>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Upload Gambar</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <button class="btn btn-primary">Tambah Fasilitas</button>
-                </div>
-            </div>
+                <button type="submit" onclick="location.href='?p=profile'" class="btn btn-primary">Buat Kosan</button>
+            </form>
         </div>
 
-
-        <button type="submit" onclick="location.href='?p=profile'" class="btn btn-primary">Buat Kosan</button>
-    </form>
+        <!-- gambar dan fasilitas -->
+        <div class="col" id="fasilitas-gambar">
+            <label>Fasilitas</label>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Fasiitas</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button class="btn btn-primary" id="tambah-fasilitas">Tambah Fasilitas</button>
+            <br>
+            <label>Gambar</label>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Gambar</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button class="btn btn-primary">Tambah Gambar</button>
+        </div>
+    </div>
 </div>
+
+<script src="./js/create-kosan.js" type="text/javascript"></script>
