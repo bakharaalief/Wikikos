7731a53c83fce0f07bac1f6216947f97c1d261f6
Zhirobyte <faizhanafi90@gmail.com>
2021-04-10 22:08:21 +0700

    membuat class user

---
+++ class/class.User.php
@@ -0,0 +1,41 @@
+<?php
+
+class User{
+    private $idUser;
+    private $username;
+    private $password;
+    private $email;
+    private $nama;
+    private $NIK;
+    private $level;
+
+    //constructor
+    public function __construct(
+        $idUser,
+        $username,
+        $password,
+        $email,
+        $nama,
+        $NIK,
+        $level,
+    ){
+        $this->idUser = $idUser;
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+        $this->nama = $nama;
+        $this->NIK = $NIK;
+        $this->level = $idUser;
+    }
+
+    //automatic create get
+    public function __get($atribute){
+        if(property_exists($this, $atribute)){
+            return $this->$atribute;
+        }
+    }
+
+    
+}
+
+?>
