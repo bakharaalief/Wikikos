3e8d3c0b36300ffea934f486d9a6c491627fd0be
Zhirobyte <faizhanafi90@gmail.com>
2021-04-16 22:49:46 +0700

    --- Auto-Git Commit ---

--- class/class.Foto_Kosan.php
+++ class/class.Foto_Kosan.php
@@ -3,14 +3,13 @@ class Foto_Kosan
 {
     private $Foto;
     private $idKosan;
- 
 
     public function __construct($Foto,  $idKosan)
     {
         $this->Foto = $Foto;
         $this->idKosan = $idKosan;
     }
-
+    
     //automatic create get
     public function __get($atribute)
     {
