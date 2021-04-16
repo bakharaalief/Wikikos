4f761da2b54ba429c118745f4fce23a61ceb6a3c
Zhirobyte <faizhanafi90@gmail.com>
2021-04-16 22:26:38 +0700

    menambahkan class foto_kosan

---
+++ class/class.Foto_Kosan.php
@@ -0,0 +1,21 @@
+<?php
+class Foto_Kosan
+{
+    private $Foto;
+    private $idKosan;
+ 
+
+    public function __construct($Foto,  $idKosan)
+    {
+        $this->Foto = $Foto;
+        $this->idKosan = $idKosan;
+    }
+
+    //automatic create get
+    public function __get($atribute)
+    {
+        if (property_exists($this, $atribute)) {
+            return $this->$atribute;
+        }
+    }
+}
