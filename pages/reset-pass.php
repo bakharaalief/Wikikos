3bb54cba1e6f5cef2c9d66d6736613be73358a9a
Zhirobyte <faizhanafi90@gmail.com>
2021-04-18 15:31:52 +0700

    --- Auto-Git Commit ---

--- pages/reset-pass.php
+++ pages/reset-pass.php
@@ -1,5 +1,14 @@
-<?php
+<div class="resetpass">
+    <div id="reserbyemail" class="container">
+    
+        <form action="./action/reset-pass/reset-pass-success.php" method="Post">
+                 
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" placeholder="Email" name="username" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
 
-
-
-?>
+    </div>    
+</div>
