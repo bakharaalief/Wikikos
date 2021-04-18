765e807dfb85bc97bd028f5a9d6db51cddaa266c
Zhirobyte <faizhanafi90@gmail.com>
2021-04-19 00:50:24 +0700

    rev page reset password

--- pages/reset-pass.php
+++ pages/reset-pass.php
@@ -1,6 +1,7 @@
 <div class="resetpass">
     <div id="resetbyemail" class="container">
         <div class="row align-items-center">
+            <!-- ini adalah page form untuk pengisian reset pass -->
             <form action="./action/pass/reset-pass-success.php" method="Post">
                 <h1>Input E-Mail</h1>
                 <input type="email" class="form-control" placeholder="Masukkan Email anda disini" name="email" required>
