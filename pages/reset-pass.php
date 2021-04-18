0541c8276f4d3a6b9e7dfb3f0508934bcd6f0621
Zhirobyte <faizhanafi90@gmail.com>
2021-04-18 23:10:46 +0700

    memperbaiki css resetpass

--- pages/reset-pass.php
+++ pages/reset-pass.php
@@ -1,17 +1,21 @@
 <div class="resetpass">
     <div id="resetbyemail" class="container">
+        <div class="inputemail">
+            <h1>Input E-Mail</h1>
+        </div>
         <form action="./action/pass/reset-pass-success.php" method="Post">
             <div class="form-group">
-                <div class="inputemail">
-                    <label>Input E-Mail</label>
-                    <div id="inputemail">
-                        <input type="text" class="form-control" placeholder="Email" name="email" required>
-                    </div>
+
+
+                <div id="inputemail">
+                    <input type="text" class="form-control" placeholder="Email" name="email" required>
                 </div>
             </div>
-            <div class="submitreset">
-                <button type="submit" class="btn btn-primary">SUBMIT</button>
-            </div>
-        </form>
     </div>
+    <div class="submitreset">
+        <button type="submit" class="btn btn-primary">SUBMIT</button>
+    </div>
+    </form>
+
+</div>
 </div>
