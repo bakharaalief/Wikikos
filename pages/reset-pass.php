617d681ae359f912e5ead093c1423fdc08049c8b
Zhirobyte <faizhanafi90@gmail.com>
2021-04-19 00:38:20 +0700

    --- Auto-Git Commit ---

--- pages/reset-pass.php
+++ pages/reset-pass.php
@@ -1,14 +1,11 @@
 <div class="resetpass">
     <div id="resetbyemail" class="container">
-        <form action="./action/pass/reset-pass-success.php" method="Post">
-
-            <h1>Input E-Mail</h1>
-            <div class="inputemail">
-                <input type="text" class="kotakinput" placeholder="Email" name="username" required>
-            </div>
-            <button class="submitreset">Submit</button>
+        <div class="row align-items-center">
+            <form action="./action/pass/reset-pass-success.php" method="Post">
+                <h1>Input E-Mail</h1>
+                <input type="email" class="form-control" placeholder="Masukkan Email anda disini" name="email" required>
+                <button type="submit" class="submit">Submit</button>
+            </form>
+        </div>
     </div>
-    </form>
-</div>
-</div>
 </div>
