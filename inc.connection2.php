bf9cea6150aa8fc8c954f954d8c82a94fbdba529
Zhirobyte <faizhanafi90@gmail.com>
2021-06-13 04:56:23 +0700

    konfigurasi cpanel connection dengan db

--- inc.connection2.php
+++ inc.connection2.php
@@ -2,11 +2,14 @@
 
 class Connection2
 {
+
     private $servername = "localhost";
     private $username = "root";
     private $password = "";
     private $dbname = "wikikos_db";
     public $conn;
+
+
 
     function __construct()
     {
@@ -17,4 +20,29 @@ class Connection2
     {
         $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
     }
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ///////////////////////////////////
+
+    // private $servername = "localhost";
+    // private $username = "wikikosm_satu";
+    // private $password = "Wikikos123456";
+    // private $dbname = "wikikosm_satu";
+    // public $conn;
+
+    // function __construct()
+    // {
+    //     $this->connect();
+    // }
+
+    // function connect()
+    // {
+
+    // $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+
+    // }
+    // pakai ini kalau mau ngetes database dengan hosting pc 
+    //========================================================
+
+
 }
